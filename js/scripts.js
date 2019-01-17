c436d83d5b4195724e38eb8f33ee3c129c10069b
Student <example@example.com>
2019-01-17 14:17:49 -0800

    added morgan freeman, john cena, and justin bieber. add if, else if, else statments

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,28 @@
 $(document).ready(function() {
   $("form#info").submit(function(event) {
-    var name = $("#nm").val();
-    $(".results").text(name);
-    $("#results").show();
-
-
     var gender = $("select#gender").val();
+    var age = parseInt($("input#age").val());
+    var name = $("#nm").val();
+    $(".congratulations").text(name);
+    $("#congratulations").show();
+
+    if(gender = 'female' && age <= 26) {
+      $("#justinBieber").show();
+      $("#johnCena").hide();
+      $("#morganFreeman").hide();
+    }
+    else if(gender = 'female' && age >= 60) {
+      $("#morganFreeman").show();
+      $("#johnCena").hide();
+      $("#justinBieber").hide();
+    }
+    else {
+      $("#johnCena").show();
+      $("#justinBieber").hide();
+      $("#morganFreeman").hide();
+    }
+
+
     event.preventDefault();
   });
 });
